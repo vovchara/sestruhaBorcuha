9c7c68c0970566b0fc217ac36dacc0ba7308b670
Nikolai Lomonosov <nlomonosov@playtika.com>
2020-12-08 12:02:31 +0200

    autofac 3

---
+++ Windows/Src/Infra/ViewFactory.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using Scene.Src.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scene.Src.Infra
+{
+   public class ViewFactory
+    {
+        private readonly ILifetimeScope _scope;
+
+        public ViewFactory(ILifetimeScope scope)
+        {
+            _scope = scope;
+        }
+        public T CreateView<T>()
+    where T : PopupBase
+        {
+            return _scope.Resolve<T>();
+        }
+    }
+}
